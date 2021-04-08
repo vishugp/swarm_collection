a8b952d70fd78417565b5895dd0c1b9643b65608
v1sh0 <f20181048@hyderabad.bits-pilani.ac.in>
2021-04-08 16:41:46 +0530

    Added Can Model Files and User-defined Gripper Arm Angle Input Controller gripper_control.py

--- src/gripper_control.py
+++ src/gripper_control.py
@@ -3,49 +3,32 @@
 import rospy
 from std_msgs.msg import Float64
 
-def talker():
+def arm_angler():
     pi=3.142
-    pub1 = rospy.Publisher('/firefly/firefly/arm1_joint_position_controller/command', Float64, queue_size=10)
-    pub2 = rospy.Publisher('/firefly/firefly/arm2_joint_position_controller/command', Float64, queue_size=10)
-    pub3 = rospy.Publisher('/firefly/firefly/arm3_joint_position_controller/command', Float64, queue_size=10)
+    pub1 = rospy.Publisher('/firefly1/firefly/arm1_joint_position_controller/command', Float64, queue_size=10)
+    pub2 = rospy.Publisher('/firefly1/firefly/arm2_joint_position_controller/command', Float64, queue_size=10)
+    pub3 = rospy.Publisher('/firefly1/firefly/arm3_joint_position_controller/command', Float64, queue_size=10)
 
     rospy.init_node('gripper_control', anonymous=False)
     rate = rospy.Rate(1000)
-    rospy.set_param('angle',0)
+    t1 = 0.00
     while not rospy.is_shutdown():
-       instr = rospy.get_param('angle')
-
-       if instr==0:
-           ## Goes from 0 to pi/8
-           theta = 0.00
-           print("Initial Angle: ",theta)
-           for i in range(0,100):
-               theta = theta+pi/800
-               print("Iteration: ",i," Angle= ",theta*180/3.142)
-               pub1.publish(theta)
-               pub2.publish(theta)
-               pub3.publish(theta)
-               rospy.sleep(0.01)
-        			   #100 iter.. totally takes 1 sec to go from 0 to pi/8
-           rospy.set_param('angle',1)
-           rate.sleep()
-       elif instr==1:
-           ## Goes from pi/8 to 0.
-           theta = pi/8;
-           print("Initial Angle: ",theta)
-           for i in range(0,100):
-               theta = theta-pi/800
-               print("Iteration: ",i," Angle= ",theta*180/3.142)
-               pub1.publish(theta)
-               pub2.publish(theta)
-               pub3.publish(theta)
-               rospy.sleep(0.01)			#100 iter.. totally takes 1 sec to go from pi/8 to 0.
-           rospy.set_param('angle',0)
-           rate.sleep()
+        t2 = float(input("Enter Angle: "))
+        t2 = (t2*pi)/180
+        diff = t2-t1
+        print("Initial Angle: ",t1)
+        for i in range(0,100):
+            t1+=(diff/100)
+            print("Iteration: ",i," Angle= ",t1*180/3.142)
+            pub1.publish(t1)
+            pub2.publish(t1)
+            pub3.publish(t1)
+            rospy.sleep(0.01)
+        t1=t2
 
 
 if __name__ == '__main__':
    try:
-       talker()
+       arm_angler()
    except rospy.ROSInterruptException:
        pass
